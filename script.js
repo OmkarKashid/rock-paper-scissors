00badc9e940fe83851ff1d8093855aebc901fd77
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-03-23 15:41:14 +0530

    Add playRound function

--- script.js
+++ script.js
@@ -3,4 +3,41 @@ function getComputerChoice(arr){
     let choice = Math.floor(Math.random() * arr.length);
     return arr[choice];
 }
-console.log(getComputerChoice(arr));
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.trim().toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    if(playerSelection == computerSelection){
+        return "It's a TIE! play another round!"
+    }
+    else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "SCISSORS" && computerSelection == "ROCK")){
+        if (playerSelection == "ROCK"){
+            return "Scissors can't cut ROCK, Player WINS!"
+        }
+        else{
+            return "Scissors can't cut ROCK, Computer WINS!"
+        }
+    }
+    else if ((playerSelection == "ROCK" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "ROCK")){
+        if(playerSelection == "PAPER"){
+            return "PAPER beats Rock, Player WINS!"
+        }
+        else{
+            return "PAPER beats Rock, Computer WINS!"
+        }
+    }
+    else if ((playerSelection == "SCISSORS" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "SCISSORS")){
+        if(playerSelection == "SCISSORS"){
+            return "SCISSORS cut Paper, Player WINS!"
+        }
+        else{
+            return "SCISSORS cut Paper, Computer WINS!"
+        }
+    }
+    else{
+        return "Invalid Choice, Player Focus!"
+    }
+    
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(arr);
+console.log(playRound(playerSelection, computerSelection));
