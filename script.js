db7dd3318da7b21e3658d7f1eec801ce410c58dc
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-03-23 16:13:22 +0530

    Add game function Game function allows player to play 5 rounds and see current score

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const arr = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice(arr){
     let choice = Math.floor(Math.random() * arr.length);
     return arr[choice];
@@ -11,25 +13,31 @@ function playRound(playerSelection, computerSelection){
     }
     else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "SCISSORS" && computerSelection == "ROCK")){
         if (playerSelection == "ROCK"){
+            playerScore++;
             return "Scissors can't cut ROCK, Player WINS!"
         }
         else{
+            computerScore++;
             return "Scissors can't cut ROCK, Computer WINS!"
         }
     }
     else if ((playerSelection == "ROCK" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "ROCK")){
         if(playerSelection == "PAPER"){
+            playerScore++;
             return "PAPER beats Rock, Player WINS!"
         }
         else{
+            computerScore++;
             return "PAPER beats Rock, Computer WINS!"
         }
     }
     else if ((playerSelection == "SCISSORS" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "SCISSORS")){
         if(playerSelection == "SCISSORS"){
+            playerScore++;
             return "SCISSORS cut Paper, Player WINS!"
         }
         else{
+            computerScore++;
             return "SCISSORS cut Paper, Computer WINS!"
         }
     }
@@ -38,6 +46,31 @@ function playRound(playerSelection, computerSelection){
     }
     
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice(arr);
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    for (let i = 1; i <= 5; i++){
+        let playerSelection = prompt("Choose Rock, Paper or Scissors");
+        let computerSelection = getComputerChoice(arr);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Current Score: Player-${playerScore} , Computer-${computerScore}`);
+    }
+    let winner = checkWinner();
+    if (winner == "Tie"){
+        console.log("It's a Tie!");
+    } 
+    else {
+        console.log(`${winner} Wins!`);
+    }
+}
+function checkWinner(){
+    if(playerScore > computerScore){
+        return "Player";
+    }
+    else if(computerScore > playerScore){
+        return "Computer";
+    }
+    else{
+        return "Tie";
+    }
+}
+
+game();
